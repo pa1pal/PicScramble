02a50aa4a00b3bf2a7163ac7d566fb620da015e2
pa1pal <pawanpal004@gmail.com>
2017-04-11 20:58:14 +0530

    added new menu screen

---
+++ app/src/main/java/pa1pal/picscramble/Menu.java
@@ -0,0 +1,31 @@
+package pa1pal.picscramble;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import pa1pal.picscramble.main.MainActivity;
+
+public class Menu extends AppCompatActivity{
+
+    @BindView(R.id.highscore)
+    Button highScore;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.newgame)
+    void newGame(){
+        Intent newGameIntent = new Intent(this, MainActivity.class);
+        startActivity(newGameIntent);
+    }
+}
